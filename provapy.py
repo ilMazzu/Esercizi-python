2f0f9cb1151eb77acc756d420794b98dd949649e
ilMazzu <mazzuv3@gmail.com>
2024-11-26 10:35:23 +0100

    primo comit

---
+++ provapy.py
@@ -0,0 +1,17 @@
+
+i = 5
+o = 0
+while i == 5:
+    import random
+    numero1 = random.randint(0, 10000000)   
+    numero2 = random.randint(0, 10000000)
+    o += 1
+    if numero1 != numero2:
+        print(numero1, numero2)
+       
+    else:        
+        print(numero1, "=", numero2)
+        i = 4
+
+print("fine")
+print(o, "tentativi")
