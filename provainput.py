bcce30d936c1357380192676676f20105f5f7cd8
ilMazzu <mazzuv3@gmail.com>
2024-11-26 11:58:52 +0100

    primo commit

---
+++ provainput.py
@@ -0,0 +1,7 @@
+nome = input("inserisci il tuo nome: ") #inserisci il nome
+cognome = input("inserisci il tuo cognome: ")
+anno = int(input("inserisci l'anno di nascita: "))
+age = 2050 - anno #calcolo dell'età nel 2050
+
+print("nel 2050 avrai", age, "anni") 
+print(nome, cognome)
