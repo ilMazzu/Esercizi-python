226650e6777b63c65ada01b5ef2b3e9fca1828c9
ilMazzu <mazzuv3@gmail.com>
2024-11-26 12:05:00 +0100

    primo commit

---
+++ provaif.py
@@ -0,0 +1,9 @@
+print("divisione")
+num1 = int(input("inserisci il primo numero: "))
+num2 = int(input("inserisci il secondo numero: "))
+
+if num2 == 0:
+    print("operazione impossibile :(")
+else:
+    print("il risultato della divisione è: ", num1 / num2)
+    
